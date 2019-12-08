02657909ce91412a8947a357acf393b0529d19a9
Alwkri <52600747+alwkri@users.noreply.github.com>
2019-12-08 05:05:39 +0300

    Create RPi GPIO .py

---
+++ RPi GPIO .py
@@ -0,0 +1,20 @@
+#physcal 11
+#BCM 17
+
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+
+while True:
+    GPIO.output(11,1)
+    print ("ok on")
+    sleep(2)
+    GPIO.output(11,0)
+    print ("ok offf")
+    sleep(2)
+    
+
+
+
